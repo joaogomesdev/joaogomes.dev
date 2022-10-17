b685d90ac5a9166510796022ea56f9840c244bc7
Joao Gomes <joaopfg.2002@gmail.com>
2022-10-17 20:52:08 +0100

    Disable some things

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,18 +6,17 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
+  // DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.preprocess(
-    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-    // Since NextAuth automatically uses the VERCEL_URL if present.
-    (str) => process.env.VERCEL_URL ?? str,
-    // VERCEL_URL doesnt include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
-  ),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  // NEXTAUTH_SECRET: z.string(),
+  // NEXTAUTH_URL: z.preprocess(
+  //   // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+  //   // Since NextAuth automatically uses the VERCEL_URL if present.
+  //   (str) => process.env.VERCEL_URL ?? str,
+  //   // VERCEL_URL doesnt include `https` so it cant be validated as a URL
+  //   process.env.VERCEL ? z.string() : z.string().url(),
+  // ),
+  // DISCORD_CLIENT_SECRET: z.string(),
 });
 
 /**
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { trpc } from "../utils/trpc";
+// import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+  // const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -50,10 +50,10 @@ const Home: NextPage = () => {
             documentation="https://www.prisma.io/docs/"
           />
         </div>
-        <div className="flex w-full items-center justify-center pt-6 text-2xl text-blue-500">
+        {/* <div className="flex w-full items-center justify-center pt-6 text-2xl text-blue-500">
           {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
         </div>
-        <AuthShowcase />
+        <AuthShowcase /> */}
       </main>
     </>
   );
@@ -61,30 +61,30 @@ const Home: NextPage = () => {
 
 export default Home;
 
-const AuthShowcase: React.FC = () => {
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
+// const AuthShowcase: React.FC = () => {
+//   const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
 
-  const { data: sessionData } = useSession();
+//   const { data: sessionData } = useSession();
 
-  return (
-    <div className="flex flex-col items-center justify-center gap-2">
-      {sessionData && (
-        <p className="text-2xl text-blue-500">
-          Logged in as {sessionData?.user?.name}
-        </p>
-      )}
-      {secretMessage && (
-        <p className="text-2xl text-blue-500">{secretMessage}</p>
-      )}
-      <button
-        className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div className="flex flex-col items-center justify-center gap-2">
+//       {sessionData && (
+//         <p className="text-2xl text-blue-500">
+//           Logged in as {sessionData?.user?.name}
+//         </p>
+//       )}
+//       {secretMessage && (
+//         <p className="text-2xl text-blue-500">{secretMessage}</p>
+//       )}
+//       <button
+//         className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
+//         onClick={sessionData ? () => signOut() : () => signIn()}
+//       >
+//         {sessionData ? "Sign out" : "Sign in"}
+//       </button>
+//     </div>
+//   );
+// };
 
 type TechnologyCardProps = {
   name: string;
