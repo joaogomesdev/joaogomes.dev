04e35fd7a50e98582b73a9d40078a37c6f3721f6
Joao Gomes <joaopfg.2002@gmail.com>
2022-11-02 09:28:47 +0000

    fix: Overflow X

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
       </Head>
-      <body className="w-screen text-gray-900 dark:text-zinc-50 bg-gray-50 dark:bg-zinc-900 font-display">
+      <body className="w-screen overflow-x-hidden text-gray-900 dark:text-zinc-50 bg-gray-50 dark:bg-zinc-900 font-display">
         <Main />
         <NextScript />
       </body>
