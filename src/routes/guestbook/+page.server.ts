f2c1fd8014e7b8fa638845cd7c0590d78eefd57a
João Gomes <joaopfg.2002@gmail.com>
2023-04-15 03:06:41 +0100

    Revert "feat(guestbook): handle success and error"

--- src/routes/guestbook/+page.server.ts
+++ src/routes/guestbook/+page.server.ts
@@ -1,9 +1,8 @@
 import { getAllSignatures, saveSignature } from '$lib/services/signatures';
 import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
-import type { PageLoad } from '../$types';
 
-export const load: PageLoad = async ({ depends }) => {
+export const load = async ({ depends }) => {
 	const signatures = await getAllSignatures();
 
 	depends('app:signatures');
@@ -17,19 +16,22 @@ export const actions: Actions = {
 		const username = String(formData.get('username'));
 		const message = String(formData.get('message'));
 
-		if (!username || typeof username !== 'string') {
-			return fail(400, { username, missing: true });
+		if (!username) {
+			return fail(400, {
+				message,
+				missing: true
+			});
 		}
 
-		if (!message || typeof message !== 'string') {
-			return fail(400, { message, missing: true });
+		if (!message) {
+			return fail(400, {
+				message,
+				missing: true
+			});
 		}
 
-		try {
-			await saveSignature(username, message);
-			return { success: true };
-		} catch (err: any) {
-			return fail(400, { error: true, message: err.message });
-		}
+		await saveSignature(username, message);
+
+		return { success: true };
 	}
 };
