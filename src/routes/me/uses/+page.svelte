8262956f2a809df67bb367b12789c1ae5fea839c
Joao Gomes <joaopfg.2002@gmail.com>
2023-04-19 00:05:24 +0100

    update uses items

--- src/routes/me/uses/+page.svelte
+++ src/routes/me/uses/+page.svelte
@@ -1,186 +1,180 @@
 <script lang="ts">
-    import setup from '$lib/assets/setup.png'
+	import setup from '$lib/assets/setup.png';
+	import UseItem from '../../../components/UseItem.svelte';
 </script>
 
 <div class="w-full">
-    <div>
-      <div>
-        <h1 class="font-bold text-4xl">What i use.</h1>
-        <p class="text-lg text-zinc-400 mt-3">Things I use in my daily life and bring me productivity at work. I decided to build this part of <span class="text-yellow-500 font-semibold">joaogomes.dev</span> because i realy like to share and give a possible tip to other devs. ✌️</p>
-      </div>
-      <div class="mt-8">
-        <img alt="My great setup" src={setup} width={900} height={600} class="rounded-2xl" />
-      </div>
-    </div>
+	<div>
+		<div>
+			<h1 class="font-bold text-4xl">What i use.</h1>
+			<p class="text-lg text-zinc-400 mt-3">
+				Things I use in my daily life and bring me productivity at work. I decided to build this
+				part of <span class="text-yellow-500 font-semibold">joaogomes.dev</span> because i realy like
+				to share and give a possible tip to other devs. ✌️
+			</p>
+		</div>
+		<div class="mt-8">
+			<img alt="My great setup" src={setup} width={900} height={600} class="rounded-2xl" />
+		</div>
+	</div>
 
-    <div class="mt-10">
-      <h2 class="text-2xl font-bold text-indigo-500">Desk.</h2>
-      <p class="text-lg text-zinc-400 mt-3">Thinks that i have on my desk at home were i spend time grinding on my Code Career 👆</p>
-      <div class="ml-10 mt-2 text-lg">
-        <li class=""><a href="" class="hover:underline cursor-pointer">Ikea desk</a></li>
-        <li class=""><a href="" class="hover:underline cursor-pointer">Ikea monitor stand</a></li>
-        <li class=""><a href="" class="hover:underline cursor-pointer">Alpha Gamer Nebula chair</a></li>
-        <li class="">Regular mousepad</li>
-        <li class="">Regular lamp</li>
-        <li class=""><a href="" class="hover:underline cursor-pointer">Headset SteelSeries Arctis 3</a></li>
-        <li class=""><a href="" class="hover:underline cursor-pointer">Samsung 24” curved monitor</a></li>
-        <li class=""><a href="" class="hover:underline cursor-pointer">Keykron K2 mechanical keyboard</a></li>
-      </div>
-    </div>
+	<div class="mt-10">
+		<h2 class="text-2xl font-bold text-indigo-500">Desk.</h2>
+		<p class="text-lg text-zinc-400 mt-3">
+			Thinks that i have on my desk at home were i spend time grinding 👆
+		</p>
+		<div class="ml-10 mt-2 text-lg">
+			<UseItem link="https://www.ikea.com/pt/pt/p/linnmon-adils-mesa-branco-s29932181/" name="Ikea desk" />
+			<UseItem link="https://www.ikea.com/pt/pt/p/elloven-suporte-do-monitor-c-gaveta-antracite-00485101/" name="Ikea monitor stand" />
+			<UseItem link="https://alpha-gamer.com/product/alpha-gamer-nebula-gaming-chair/" name="Alpha Gamer Nebula chair" />
+			<UseItem name="Regular mousepad" />
+			<UseItem name="Regular lamp" />
+			<UseItem link="https://steelseries.com/gaming-headsets/arctis-3" name="Headset SteelSeries Arctis 3" />
+			<UseItem link="https://www.worten.pt/produtos/monitor-curvo-samsung-lc24f396fhrxen-24-full-hd-va-freesync-6554794" name="Samsung 24” curved monitor" />
+			<UseItem link="https://www.keychron.com/products/keychron-k2-wireless-mechanical-keyboard" name="Keykron K2 mechanical keyboard" />
+		</div>
+	</div>
 
-    <div class="mt-10">
-      <h2 class="text-2xl font-bold text-indigo-500">Dev Backpack.</h2>
-      <p class="text-lg text-zinc-400 mt-3">Thinks that i carry everyday with me on my Backpack 🎒</p>
-      <div class="ml-10 mt-2 text-lg">
-        <li><a href="" class="hover:underline cursor-pointer">Amazon Backpack</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">Macbook M1 Air - 16 Ram - 251 Storage</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">Iphone 7</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">Magic Mouse 2</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">UGreen 7 Port hub</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">Powerbank</a></li>
-        <li><a>Laptop sleeve</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">2nd generation Apple Airpods</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">Wire JBL earphones</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">Regular mousepad</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">USB Lightning cable</a></li>
-        <li><a>USB C cable</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">USB regular cable</a></li>
-        <li><a href="" class="hover:underline cursor-pointer">Reuseable water bottle</a></li>
-      </div>
-    </div>
+	<div class="mt-10">
+		<h2 class="text-2xl font-bold text-indigo-500">Backpack.</h2>
+		<p class="text-lg text-zinc-400 mt-3">Thinks that i carry everyday with me on my 🎒</p>
+		<div class="ml-10 mt-2 text-lg">
+			<UseItem name="Amazon Backpack" />
+			<UseItem name="M1 Macbook Air">- 16 Ram - 251 Storage</UseItem>
+			<UseItem name="Iphone 7" />
+			<UseItem name="Magic Mouse 2" />
+			<UseItem name="UGreen 7 Port hub" />
+			<UseItem name="Powerbank" />
+			<UseItem name="Laptop sleeve" />
+			<UseItem name="Apple Airpods 2nd generation" />
+			<UseItem name="Apple Airpods Pro 2 generation" />
+			<UseItem name="Regular mousepad" />
+			<UseItem name="USB Lightning cable" />
+			<UseItem name="USB-C cable" />
+			<UseItem name="USB regular cable" />
+			<UseItem name="Reuseable water bottle" />
+		</div>
+	</div>
 
-    <div class="mt-10">
-      <h2 class="text-2xl font-bold text-indigo-500">Laptop apps.</h2>
-      <p class="text-lg text-zinc-400 mt-3">Mac apps that i use on my workflow 💻</p>
-      <div class="ml-10 mt-2 text-lg">
-        <li><a class="hover:underline cursor-pointer">ICalendar</a></li>
-        <li><a class="hover:underline cursor-pointer">Apple Notes</a></li>
-        <li><a class="hover:underline cursor-pointer">Notion</a></li>
-        <li><a class="hover:underline cursor-pointer">Obsidian</a></li>
-        <li><a class="hover:underline cursor-pointer">Centered</a></li>
-        <li><a class="hover:underline cursor-pointer">Min Browser</a></li>
-        <li><a class="hover:underline cursor-pointer">Raycast</a></li>
-        <li><a class="hover:underline cursor-pointer">Tiles</a></li>
-        <li><a class="hover:underline cursor-pointer">AltTab</a></li>
-        <li><a class="hover:underline cursor-pointer">Authy</a></li>
-        <li><a class="hover:underline cursor-pointer">Clipy</a></li>
-        <li><a class="hover:underline cursor-pointer">Discord</a></li>
-        <li><a class="hover:underline cursor-pointer">Gifski</a></li>
-        <li><a class="hover:underline cursor-pointer">Apple Mail</a></li>
-        <li><a class="hover:underline cursor-pointer">Spotify</a></li>
-        <li><a class="hover:underline cursor-pointer">OBS Studio</a></li>
-        <li><a class="hover:underline cursor-pointer">Telegram</a></li>
-        <li><a class="hover:underline cursor-pointer">Zoom</a></li>
-        <li><a class="hover:underline cursor-pointer">Record it</a></li>
-        <li><a class="hover:underline cursor-pointer">Itsycal</a></li>
-        <li><a class="hover:underline cursor-pointer">Qmoji</a></li>
-      </div>
-    </div>
+	<div class="mt-10">
+		<h2 class="text-2xl font-bold text-indigo-500">Laptop apps.</h2>
+		<p class="text-lg text-zinc-400 mt-3">Mac apps that i use on my workflow 💻</p>
+		<div class="ml-10 mt-2 text-lg">
+			<UseItem name="Cron Calendar" />
+			<UseItem name="Apple Notes" />
+			<UseItem name="Notion" />
+			<UseItem name="Arc Browser" />
+			<UseItem name="Raycast" />
+			<UseItem name="Authy" />
+			<UseItem name="Clipy" />
+		</div>
+	</div>
 
-    <div class="mt-10">
-      <h2 class="text-2xl font-bold text-indigo-500">Mobile apps.</h2>
-      <p class="text-lg text-zinc-400 mt-3">Mobile apps that i have installed on my Iphone 7 📱</p>
-      <div class="ml-10 mt-2 text-lg">
-        <li><a class="hover:underline cursor-pointer">Youtube </a></li>
-        <li><a class="hover:underline cursor-pointer">YT Music</a></li>
-        <li><a class="hover:underline cursor-pointer">WhatsApp</a></li>
-        <li><a class="hover:underline cursor-pointer">Twitter</a></li>
-        <li><a class="hover:underline cursor-pointer">Twitch</a></li>
-        <li><a class="hover:underline cursor-pointer">Apple TV</a></li>
-        <li><a class="hover:underline cursor-pointer">Telegram</a></li>
-        <li><a class="hover:underline cursor-pointer">Spotify</a></li>
-        <li><a class="hover:underline cursor-pointer">Sports Go</a></li>
-        <li><a class="hover:underline cursor-pointer">Revolut</a></li>
-        <li><a class="hover:underline cursor-pointer">Caixa Directa</a></li>
-        <li><a class="hover:underline cursor-pointer">Pokemon Go</a></li>
-        <li><a class="hover:underline cursor-pointer">Organizze</a></li>
-        <li><a class="hover:underline cursor-pointer">Notion</a></li>
-        <li><a class="hover:underline cursor-pointer">My Vodafone</a></li>
-        <li><a class="hover:underline cursor-pointer">Moovit</a></li>
-        <li><a class="hover:underline cursor-pointer">MB Way</a></li>
-        <li><a class="hover:underline cursor-pointer">Instagram</a></li>
-        <li><a class="hover:underline cursor-pointer">Google Calendar</a></li>
-        <li><a class="hover:underline cursor-pointer">Gmail</a></li>
-        <li><a class="hover:underline cursor-pointer">Expo Go</a></li>
-        <li><a class="hover:underline cursor-pointer">Discord</a></li>
-        <li><a class="hover:underline cursor-pointer">Dev</a></li>
-        <li><a class="hover:underline cursor-pointer">Authy</a></li>
-      </div>
-    </div>
+	<div class="mt-10">
+		<h2 class="text-2xl font-bold text-indigo-500">Mobile apps.</h2>
+		<p class="text-lg text-zinc-400 mt-3">Mobile apps that i have installed on my Iphone 7 📱</p>
+		<div class="ml-10 mt-2 text-lg">
+			<UseItem name="YT Music" />
+			<UseItem name="Spotify" />
+			<UseItem name="MB Way" />
+			<UseItem name="Notion" />
+			<UseItem name="Sports Go" />
+			<UseItem name="Moovit" />
+			<UseItem name="Expo Go" />
+			<UseItem link="https://apps.apple.com/us/app/twilio-authy/id494168017" name="Authy" />
+			<UseItem name="WhatsApp" />
+			<UseItem link="https://apps.apple.com/us/app/reddit/id1064216828" name="Reddit" />
+			<UseItem name="Tick Tick" />
+		</div>
+	</div>
+	<div class="mt-10">
+		<h2 class="text-2xl font-bold text-indigo-500">Coding.</h2>
 
-    <div class="mt-10">
-      <h2 class="text-2xl font-bold text-indigo-500">Coding.</h2>
-      <p class="text-lg text-zinc-400 mt-3">Apps and tools that i use to craft the best code in the world 👨🏻‍💻</p>
-      <div class="ml-10 mt-2 text-lg">
-        <li><a class="hover:underline cursor-pointer">Visual Studio Code</a></li>
-        <li><a class="hover:underline cursor-pointer">VSCode Night Owl Theme</a></li>
-        <li><a class="hover:underline cursor-pointer">Dark Mono font</a></li>
-        <li><a class="hover:underline cursor-pointer">ZSH </a></li>
-        <li><a class="hover:underline cursor-pointer">Spaceship theme for ZSH</a></li>
-        <li><a class="hover:underline cursor-pointer">Hyper</a></li>
-        <li><a class="hover:underline cursor-pointer">Dracula theme for Hyper</a></li>
-        <li><a class="hover:underline cursor-pointer">Fig</a></li>
-        <li><a class="hover:underline cursor-pointer">Docker</a></li>
-        <li><a class="hover:underline cursor-pointer">XCode for emulator</a></li>
-        <li><a class="hover:underline cursor-pointer">DevCleaner</a></li>
-        <li><a class="hover:underline cursor-pointer">Beekeeper Studio</a></li>
-        <li><a class="hover:underline cursor-pointer">Insomnia</a></li>
-        <li><a class="hover:underline cursor-pointer">Google Chrome browser</a></li>
-      </div>
-    </div>
+	<div class="mt-4 bg-green-900 bg-opacity-20 p-4 rounded-lg">
+		<h2 class="text-lg font-bold text-yellow-500">
+			⚡️ Becoming a blazing fast developer ⚡️
+		</h2>
+		<p class="text-lg text-zinc-400 mt-3">
+			Im on the journey of learning a workflow with Neovim, and these are the tools there im using.
+		</p>
+		<div class="ml-6 mt-2 text-lg">
+			<UseItem link="https://github.com/koekeishiya/yabai" name="Yabai">
+				- a tiling window manager.
+			</UseItem>
+			<UseItem link="https://github.com/koekeishiya/skhd" name="skhd"
+				>- a simple hotkey deamon.</UseItem
+			>
+			<UseItem link="https://github.com/tmux/tmux/wiki" name="Tmux"
+				>- a terminal session manager.</UseItem
+			>
+			<UseItem link="https://github.com/starship/starship" name="Alacritty"
+				>- a terminal.</UseItem
+			>
+			<UseItem link="https://github.com/starship/starship" name="Starship"
+				>- a awesome terminal prompt.</UseItem
+			>
+			<UseItem link="https://neovim.io/" name="NeoVim">
+				- using AstroNvim as my baseline, your can find my own custom config
+				<a class="hover:underline text-zinc-100" href="https://github.com/joaogomesdev/.nvim"
+					>here</a
+				>.
+			</UseItem>
+			<UseItem link="https://github.com/joaogomesdev/dotfiles" name="My own .dotfiles">
+				- here your can find all my configuration files for the tools that I use.</UseItem
+			>
+		</div>
+	</div>
 
-    <div class="mt-10">
-      <h2 class="text-2xl font-bold text-indigo-500">⚡️ Becoming a blazing fast developer ⚡️</h2>
-      <p class="text-lg text-zinc-400 mt-3">Im on the journey of learning a workflow with Neovim, and these are the tools im using 🛠️</p>
-      <div class="ml-10 mt-2 text-lg">
-        <li><a class="hover:underline cursor-pointer">NeoVim</a></li>
-        <li><a class="hover:underline cursor-pointer">AstroVim</a><span class="text-zinc-500"> - currently testing. Liking it 👍</span></li>
-        <li><a class="hover:underline cursor-pointer">Tmux</a></li>
-        <li><a class="hover:underline cursor-pointer">My own .dotfiles</a><span class="text-zinc-500"> - currently not using nvim because im testing AstroVim. But i want to improve my own config.</span></li>
+	<div class="mt-10">
+		<h2 class="text-2xl font-bold text-indigo-500">VSCode configuration.</h2>
+		<p class="text-lg text-zinc-400 mt-3">
+			Apps and tools that i use to craft the best code in the world 👨🏻‍💻
+		</p>
+		<div class="ml-10 mt-2 text-lg">
+			<UseItem name="Visual Studio Code" />
+			<UseItem name="Night Owl theme" />
+			<UseItem name="Dark Mono font" />
+		</div>
+	</div>
+	</div>
 
-      </div>
-    </div>
+	
 
-    <div class="mt-10">
-      <h2 class="text-2xl font-bold text-indigo-500">CLIs.</h2>
-      <p class="text-lg text-zinc-400 mt-3">Command line tools that i use on my workflow.</p>
-      <div class="ml-10 mt-2 text-lg">
-        <li><a class="hover:underline cursor-pointer">homebrew</a></li>
-        <li><a class="hover:underline cursor-pointer">gh</a> <span class="text-zinc-500"> - Github CLI</span></li>
-        <li><a class="hover:underline cursor-pointer">npm</a></li>
-        <li><a class="hover:underline cursor-pointer">yarn</a></li>
-        <li><a class="hover:underline cursor-pointer">expo</a><span class="text-zinc-500"> - Expo CLI</span></li>
-        <li><a class="hover:underline cursor-pointer">npm-quick-run</a></li>
-      </div>
-    </div>
+	<div class="mt-10">
+		<h2 class="text-2xl font-bold text-indigo-500">CLIs.</h2>
+		<p class="text-lg text-zinc-400 mt-3">Command line tools that i use on my workflow.</p>
+		<div class="ml-10 mt-2 text-lg">
+			<UseItem name="homebrew" />
+			<UseItem name="asdf" />
+			<UseItem name="nvm" />
+			<UseItem name="npm" />
+			<UseItem name="gh">- Github CLI</UseItem>
+			<UseItem name="expo">- Expo CLI</UseItem>
+		</div>
+	</div>
 
-    <div class="mt-10">
-      <h2 class="text-2xl font-bold text-indigo-500">My Code Stack.</h2>
-      <p class="text-lg text-zinc-400 mt-3">Technologies that im currently studying / using for my personal projects ✨</p>
-      <div class="ml-10 mt-2 text-lg">
-        <li><a class="hover:underline cursor-pointer">Typescript</a></li>
-        <li><a class="hover:underline cursor-pointer">React</a></li>
-        <li><a class="hover:underline cursor-pointer">Remix</a></li>
-        <li><a class="hover:underline cursor-pointer">Next</a></li>
-        <li><a class="hover:underline cursor-pointer">React Native</a></li>
-        <li><a class="hover:underline cursor-pointer">Expo</a></li>
-        <li><a class="hover:underline cursor-pointer">Prisma</a></li>
-        <li><a class="hover:underline cursor-pointer">Tailwind CSS</a></li>
-      </div>
-    </div>
+	<div class="mt-10">
+		<h2 class="text-2xl font-bold text-indigo-500">My Code Stack.</h2>
+		<p class="text-lg text-zinc-400 mt-3">
+			Technologies that im currently studying / using for my personal projects ✨
+		</p>
+		<div class="ml-10 mt-2 text-lg">
+			<UseItem name="Typescript" />
+			<UseItem name="Next" />
+			<UseItem name="Svelte">- rebuilding this website SvelteKit.</UseItem>
+			<UseItem link="https://supabase.com" name="Supabase" />
+			<UseItem link="https://prisma.io" name="Prisma" />
+			<UseItem name="Tailwind CSS" />
+			<UseItem name="React Native">- with Expo</UseItem>
+		</div>
+	</div>
 
-    <div class="mt-10">
-      <h2 class="text-2xl font-bold text-indigo-500">Services.</h2>
-      <p class="text-lg text-zinc-400 mt-3">Tech / Entertainment services that i use 💸</p>
-      <div class="ml-10 mt-2 text-lg">
-        <li><a class="hover:underline cursor-pointer">Centered</a><span class="text-zinc-500"> - Pro version because it helps getting the work done.</span></li>
-        <li><a class="hover:underline cursor-pointer">Spotify Student</a></li>
-        <li><a class="hover:underline cursor-pointer">Amazon Prime</a></li>
-        <li><a class="hover:underline cursor-pointer">Netflix</a></li>
-        <li><a class="hover:underline cursor-pointer">Vercel</a></li>
-        <li><a class="hover:underline cursor-pointer">Railway</a></li>
-        <li><a class="hover:underline cursor-pointer">Github</a></li>
-        <li><a class="hover:underline cursor-pointer">Fly.io</a></li>
-      </div>
-    </div>
-  </div>
+	<div class="mt-10">
+		<h2 class="text-2xl font-bold text-indigo-500">Services.</h2>
+		<p class="text-lg text-zinc-400 mt-3">Tech / Entertainment services that i use 💸</p>
+		<div class="ml-10 mt-2 text-lg">
+			<UseItem name="Spotify" />
+			<UseItem name="Vercel" />
+		</div>
+	</div>
+</div>
