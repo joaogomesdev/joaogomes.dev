c293b93a464019103173be1c37f7efcfc3b34151
Joao Gomes <joaopfg.2002@gmail.com>
2023-04-19 22:48:20 +0100

    feat(uses): fix writing error

--- src/routes/me/uses/+page.svelte
+++ src/routes/me/uses/+page.svelte
@@ -24,14 +24,32 @@
 			Thinks that i have on my desk at home were i spend time grinding 👆
 		</p>
 		<div class="ml-10 mt-2 text-lg">
-			<UseItem link="https://www.ikea.com/pt/pt/p/linnmon-adils-mesa-branco-s29932181/" name="Ikea desk" />
-			<UseItem link="https://www.ikea.com/pt/pt/p/elloven-suporte-do-monitor-c-gaveta-antracite-00485101/" name="Ikea monitor stand" />
-			<UseItem link="https://alpha-gamer.com/product/alpha-gamer-nebula-gaming-chair/" name="Alpha Gamer Nebula chair" />
+			<UseItem
+				link="https://www.ikea.com/pt/pt/p/linnmon-adils-mesa-branco-s29932181/"
+				name="Ikea desk"
+			/>
+			<UseItem
+				link="https://www.ikea.com/pt/pt/p/elloven-suporte-do-monitor-c-gaveta-antracite-00485101/"
+				name="Ikea monitor stand"
+			/>
+			<UseItem
+				link="https://alpha-gamer.com/product/alpha-gamer-nebula-gaming-chair/"
+				name="Alpha Gamer Nebula chair"
+			/>
 			<UseItem name="Regular mousepad" />
 			<UseItem name="Regular lamp" />
-			<UseItem link="https://steelseries.com/gaming-headsets/arctis-3" name="Headset SteelSeries Arctis 3" />
-			<UseItem link="https://www.worten.pt/produtos/monitor-curvo-samsung-lc24f396fhrxen-24-full-hd-va-freesync-6554794" name="Samsung 24” curved monitor" />
-			<UseItem link="https://www.keychron.com/products/keychron-k2-wireless-mechanical-keyboard" name="Keykron K2 mechanical keyboard" />
+			<UseItem
+				link="https://steelseries.com/gaming-headsets/arctis-3"
+				name="Headset SteelSeries Arctis 3"
+			/>
+			<UseItem
+				link="https://www.worten.pt/produtos/monitor-curvo-samsung-lc24f396fhrxen-24-full-hd-va-freesync-6554794"
+				name="Samsung 24” curved monitor"
+			/>
+			<UseItem
+				link="https://www.keychron.com/products/keychron-k2-wireless-mechanical-keyboard"
+				name="Keykron K2 mechanical keyboard"
+			/>
 		</div>
 	</div>
 
@@ -90,55 +108,51 @@
 	<div class="mt-10">
 		<h2 class="text-2xl font-bold text-indigo-500">Coding.</h2>
 
-	<div class="mt-4 bg-green-900 bg-opacity-20 p-4 rounded-lg">
-		<h2 class="text-lg font-bold text-yellow-500">
-			⚡️ Becoming a blazing fast developer ⚡️
-		</h2>
-		<p class="text-lg text-zinc-400 mt-3">
-			Im on the journey of learning a workflow with Neovim, and these are the tools there im using.
-		</p>
-		<div class="ml-6 mt-2 text-lg">
-			<UseItem link="https://github.com/koekeishiya/yabai" name="Yabai">
-				- a tiling window manager.
-			</UseItem>
-			<UseItem link="https://github.com/koekeishiya/skhd" name="skhd"
-				>- a simple hotkey deamon.</UseItem
-			>
-			<UseItem link="https://github.com/tmux/tmux/wiki" name="Tmux"
-				>- a terminal session manager.</UseItem
-			>
-			<UseItem link="https://github.com/starship/starship" name="Alacritty"
-				>- a terminal.</UseItem
-			>
-			<UseItem link="https://github.com/starship/starship" name="Starship"
-				>- a awesome terminal prompt.</UseItem
-			>
-			<UseItem link="https://neovim.io/" name="NeoVim">
-				- using AstroNvim as my baseline, your can find my own custom config
-				<a class="hover:underline text-zinc-100" href="https://github.com/joaogomesdev/.nvim"
-					>here</a
-				>.
-			</UseItem>
-			<UseItem link="https://github.com/joaogomesdev/dotfiles" name="My own .dotfiles">
-				- here your can find all my configuration files for the tools that I use.</UseItem
-			>
+		<div class="mt-4 bg-green-900 bg-opacity-20 p-4 rounded-lg">
+			<h2 class="text-lg font-bold text-yellow-500">⚡️ Becoming a blazing fast developer ⚡️</h2>
+			<p class="text-lg text-zinc-400 mt-3">
+				Im on the journey of learning a workflow with Neovim, and these are the tools there im
+				using.
+			</p>
+			<div class="ml-6 mt-2 text-lg">
+				<UseItem link="https://github.com/koekeishiya/yabai" name="Yabai">
+					- a tiling window manager.
+				</UseItem>
+				<UseItem link="https://github.com/koekeishiya/skhd" name="skhd"
+					>- a simple hotkey deamon.</UseItem
+				>
+				<UseItem link="https://github.com/tmux/tmux/wiki" name="Tmux"
+					>- a terminal session manager.</UseItem
+				>
+				<UseItem link="https://github.com/starship/starship" name="Alacritty">- a terminal.</UseItem
+				>
+				<UseItem link="https://github.com/starship/starship" name="Starship"
+					>- a awesome terminal prompt.</UseItem
+				>
+				<UseItem link="https://neovim.io/" name="NeoVim">
+					- using AstroNvim as my baseline, your can find my own custom config
+					<a class="hover:underline text-zinc-100" href="https://github.com/joaogomesdev/.nvim"
+						>here</a
+					>.
+				</UseItem>
+				<UseItem link="https://github.com/joaogomesdev/dotfiles" name="My own .dotfiles">
+					- here your can find all my configuration files for the tools that I use.</UseItem
+				>
+			</div>
+		</div>
+
+		<div class="mt-10">
+			<h2 class="text-2xl font-bold text-indigo-500">VSCode configuration.</h2>
+			<p class="text-lg text-zinc-400 mt-3">
+				Apps and tools that i use to craft the best code in the world 👨🏻‍💻
+			</p>
+			<div class="ml-10 mt-2 text-lg">
+				<UseItem name="Visual Studio Code" />
+				<UseItem name="Night Owl theme" />
+				<UseItem name="Dark Mono font" />
+			</div>
 		</div>
 	</div>
-
-	<div class="mt-10">
-		<h2 class="text-2xl font-bold text-indigo-500">VSCode configuration.</h2>
-		<p class="text-lg text-zinc-400 mt-3">
-			Apps and tools that i use to craft the best code in the world 👨🏻‍💻
-		</p>
-		<div class="ml-10 mt-2 text-lg">
-			<UseItem name="Visual Studio Code" />
-			<UseItem name="Night Owl theme" />
-			<UseItem name="Dark Mono font" />
-		</div>
-	</div>
-	</div>
-
-	
 
 	<div class="mt-10">
 		<h2 class="text-2xl font-bold text-indigo-500">CLIs.</h2>
@@ -161,7 +175,7 @@
 		<div class="ml-10 mt-2 text-lg">
 			<UseItem name="Typescript" />
 			<UseItem name="Next" />
-			<UseItem name="Svelte">- rebuilding this website SvelteKit.</UseItem>
+			<UseItem name="Svelte">- rebuilding this website with SvelteKit.</UseItem>
 			<UseItem link="https://supabase.com" name="Supabase" />
 			<UseItem link="https://prisma.io" name="Prisma" />
 			<UseItem name="Tailwind CSS" />
